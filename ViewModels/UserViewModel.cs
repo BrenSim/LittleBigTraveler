9d938af39df6d8a81860e42746b028744c4291b1
BrenSim <brendan.simon@outlook.fr>
2023-05-29 12:25:39 +0200

    BS Scrud Users/Customer/Adm/Partner in progress

---
+++ ViewModels/UserViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using LittleBigTraveler.Models.UserClasses;
+
+namespace LittleBigTraveler.ViewModels
+{
+    public class UserViewModel
+    {
+        public int UserId { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string ProfilePicture { get; set; }
+
+        public int CustomerId { get; set; }
+        public int LoyaltyPoint { get; set; }
+        public int CommentPoint { get; set; }
+
+        public int AdministratorId { get; set; }
+
+        public int PartnerId { get; set; }
+        public int RoleId { get; set; }
+        public string RoleName { get; set; }
+        public string RoleType { get; set; }
+
+        public string UserType { get; set; }
+
+        public List<UserViewModel> Users { get; set; }
+    }
+}
